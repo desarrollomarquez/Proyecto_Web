da7bc8a695345b6a6ea93cb0d72b2c73f89dee3e
Diego Maŕquez <desarrollo.marquez@gmail.com>
2018-08-11 22:52:37 -0500

    Comentario de revision

--- Backend_Spring_boot/demoapp/src/main/java/com/example/demoapp/IndexController.java
+++ Backend_Spring_boot/demoapp/src/main/java/com/example/demoapp/IndexController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class IndexController {
-
+// Revisar configuracion del prototipo inicial para spring boot
 	public String index() {
 		return "index";
 	}
