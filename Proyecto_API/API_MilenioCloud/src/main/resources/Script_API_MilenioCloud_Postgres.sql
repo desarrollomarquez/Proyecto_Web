2d35dbe1bfa0d84ae8c8322f7183bd7d2b665ba7
Diego Maŕquez <desarrollo.marquez@gmail.com>
2018-12-09 17:55:08 -0500

    Ajustes Proyecto API Gestion Educativa

---
+++ Proyecto_API/API_MilenioCloud/src/main/resources/Script_API_MilenioCloud_Postgres.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE articles (
+	 articleId SERIAL PRIMARY KEY,
+	 title VARCHAR(200) NOT NULL,
+	 category VARCHAR(100) NOT NULL
+);
+
+SELECT * FROM articles;
+
+INSERT INTO articles (title, category) VALUES ("Sprint Boot", "Backend");
