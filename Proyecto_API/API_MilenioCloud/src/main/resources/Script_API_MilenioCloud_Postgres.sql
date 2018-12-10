ab0702961a0006489cac200625023c4cfa5c9f61
Diego Maŕquez <desarrollo.marquez@gmail.com>
2018-12-09 21:03:42 -0500

    Inclusion script Postgres

---
+++ Proyecto_API/API_MilenioCloud/src/main/resources/Script_API_MilenioCloud_Postgres.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE articles (
+	 articleId SERIAL PRIMARY KEY,
+	 title VARCHAR(200) NOT NULL,
+	 category VARCHAR(100) NOT NULL
+);
+
+SELECT * FROM articles;
+
+INSERT INTO articles VALUES (1,'Sprint', 'Backend'); 
+INSERT INTO articles VALUES (2,'Sprintboot', 'Frontend');
+INSERT INTO articles VALUES (3,'Sprintbootrest', 'database'); 
