326679eafb5f317f0b3969a25acc20a6ba0b6177
Diego Maŕquez <desarrollo.marquez@gmail.com>
2018-12-10 22:07:02 -0500

    Adicion tabla Colegio al modelo

--- Proyecto_API/API_MilenioCloud/src/main/resources/Script_API_MilenioCloud_Postgres.sql
+++ Proyecto_API/API_MilenioCloud/src/main/resources/Script_API_MilenioCloud_Postgres.sql
@@ -8,4 +8,26 @@ SELECT * FROM articles;
 
 INSERT INTO articles VALUES (1,'Sprint', 'Backend'); 
 INSERT INTO articles VALUES (2,'Sprintboot', 'Frontend');
-INSERT INTO articles VALUES (3,'Sprintbootrest', 'database'); 
+INSERT INTO articles VALUES (3,'Sprintbootrest', 'database');
+
+
+CREATE TABLE COLEGIO (
+	 codigo_id SERIAL PRIMARY KEY,
+	 nit             INT  NOT NULL,
+	 nombre          VARCHAR(200) NOT NULL,
+         codigo_colegio  INT NOT NULL,
+         codigo_dane  	 INT NOT NULL,
+	 direccion       VARCHAR(100) NOT NULL,
+	 telefono        VARCHAR(20) NOT NULL,
+	 fini_fiscal     DATE NOT NULL,
+	 ffin_fiscal     DATE NOT NULL,
+	 ubicacion_geo   VARCHAR(100) NOT NULL,
+	 foto            VARCHAR(100) NOT NULL
+	 
+);
+
+
+SELECT * FROM COLEGIO
+
+
+
